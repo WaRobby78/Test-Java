08e0f1ec790fc2f27462dee2f30e96f97f6152ff
robga_000 <robga_000@Ordinateur>
2019-08-30 13:38:51 +0200

    Test commit println

---
+++ src/Test3.java
@@ -0,0 +1,24 @@
+//import java-util.*;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+		int numero=1;		//on créé la partition avec les valeurs voulues
+		String titre=" une superbe musique ";
+		String compositeur=" un talentueux musicien ";
+		String mesure="4/4";
+		String L="1/4";
+		String tonalité="A";
+		//char notes[]={'C',' ','E',' ','E',' ','E',' ','|',' ','D',' ','F',' ','F','2',' ','|',' ','G',' ','G',' ','F',' ','G',' ','|',' ','A','G','A','B',' ','c','2',' ','\n',' ','C',' ','E',' ','E',' ','E',' ','|',' ','D',' ','F',' ','F','2',' ','|',' ','G','G',' ','G',' ','F',' ','G',' ','|',' ','b',' ','A',' ','A','2',' '};
+		//il faut laisser un espace a la fin du tableau
+		
+			//on demande l'affichage de la partition sur la nouvelle fenetre
+		
+		System.out.println(titre);
+		
+		return ;
+
+	}
+
+}
